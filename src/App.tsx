d7523683081bd9ba26448b7bbfa77cab74a75a9f
Way-Maker7 <Denis_Sabou@gmx.de>
2022-07-03 01:12:23 +0200

    Inputfeld für die dynamische Suche hinzugefügt

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import MealsCard from "./components/MealsCard";
 function App() {
 
     const[mealsData, setMealsData] = useState([])
+    const[inputSearch, setInputSearch] = useState("")
 
     useEffect(() => {
-        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
             .then((res) => setMealsData(res.data.meals))
-    }, [])
+    },[inputSearch])
 
   return(
    <div className="">
      <h1>React Koch App</h1>
-     <input type="text" placeholder="Geben Sie den Namen der Speise ein (auf English bitte)"/>
+     <input type="text" placeholder="Geben Sie den Namen der Speise ein (auf English bitte)"
+     onChange={(e) => setInputSearch(e.target.value)}/>
        <div className="meals-container">
            {mealsData.map((meal) => (<MealsCard key={meal.idMeal} meal={meal}/>))}
        </div>
